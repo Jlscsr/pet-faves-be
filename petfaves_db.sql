57f25a0afae134dd386e899005a0e63d39cc3acd
Julius Raagas <92242420+Jlscsr@users.noreply.github.com>
2024-04-28 19:32:47 +0800

    Update petfaves_db.sql

--- petfaves_db.sql
+++ petfaves_db.sql
@@ -42,14 +42,6 @@ CREATE TABLE `pets_tb` (
   `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Dumping data for table `pets_tb`
---
-
-INSERT INTO `pets_tb` (`id`, `userID`, `petName`, `age`, `gender`, `petType`, `petBreed`, `petVacHistory`, `petHistory`, `petPhotoURL`, `created_at`, `updated_at`) VALUES
-(2, NULL, 'Blackie', 4, 'male', 'Dog', 'Aspin', '2022-10-28', 'qwqweqweqweqeqweqweqweqweqweqweqweqweqwe', 'https://firebasestorage.googleapis.com/v0/b/pet-faves.appspot.com/o/Dog%2FAspin%2F2024-04-28_Blackie_blackie_pic.jpg?alt=media&token=15b38ddb-1fc5-4f81-bdc3-7dcf3d1905b7', '2024-04-28 10:48:12', '2024-04-28 10:48:12'),
-(3, NULL, 'Blackie', 4, 'male', 'Dog', 'Aspin', '2022-10-28', 'I have a lot of wounds on my body and am a homeless puppy; my now-fur parent found me and took me to the shelter.', 'https://firebasestorage.googleapis.com/v0/b/pet-faves.appspot.com/o/Dog%2FAspin%2F2024-04-28_Blackie_blackie_pic.jpg?alt=media&token=34e6f3ed-08ea-4763-b035-49648fff4b4c', '2024-04-28 11:19:53', '2024-04-28 11:19:53');
-
 -- --------------------------------------------------------
 
 --
