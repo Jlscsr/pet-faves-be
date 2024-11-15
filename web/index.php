29a5c601c1dc5b2eff0c7254a7a2260dbe645852
Julius R. <raagasjuliuscaesar@gmail.com>
2024-11-15 22:33:20 +0800

    Test

--- web/index.php
+++ web/index.php
@@ -8,7 +8,9 @@ use App\API\Router;
 
 echo "Hello, World!3";
 $url = parse_url(getenv('JAWSDB_URL'));
-var_dump("$url");
+var_dump($url['user']);
+var_dump($url['pass']);
+var_dump($url['host']);
 try {
     $router = new Router();
     $router->handleRequest();
