09314e4a076affab4b218da6d68b6d377651eca8
Julius R. <raagasjuliuscaesar@gmail.com>
2024-11-15 23:01:53 +0800

    Test

--- src/Helpers/HeaderHelper.php
+++ src/Helpers/HeaderHelper.php
@@ -9,7 +9,7 @@ class HeaderHelper
         if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
             header('Access-Control-Allow-Origin: http://localhost:5173');
             header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE");
-            header("Access-Control-Allow-Headers: Content-Type, X-Requested-With");
+            header("Access-Control-Allow-Headers: Content-Type, Authorization");
             header("Access-Control-Allow-Credentials: true");
             header("Access-Control-Expose-Headers: Content-Length");
 
@@ -20,9 +20,15 @@ class HeaderHelper
     public static function setResponseHeaders()
     {
         header("Access-Control-Allow-Origin: http://localhost:5173");
-        header('Access-Control-Allow-Credentials: true');
         header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE");
         header("Access-Control-Allow-Headers: Content-Type, Authorization");
+        header('Access-Control-Allow-Credentials: true');
         header('Content-Type: application/json');
+        header("Access-Control-Expose-Headers: Content-Length");
+
+        if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+            http_response_code(200);
+            exit();
+        }
     }
 }
