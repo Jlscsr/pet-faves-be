abfa56bef9906e5615ee6e2a1f545963f8c061b3
Julius R. <raagasjuliuscaesar@gmail.com>
2024-11-16 01:30:10 +0800

    aojsdlk;las

--- web/index.php
+++ web/index.php
@@ -9,13 +9,10 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 try {
-    $url = $_GET['url'] ?? '/';  // Capture the routed URL
     $uri = $_SERVER['REQUEST_URI'];
-    echo "URI: $uri";  // Debug the URI
-    echo "Routing URL: $url";  // Debug the captured URL
 
     $router = new Router();
-    $router->handleRequest($url);  // Pass the URL to your Router logic
+    $router->handleRequest($uri);  // Pass the URL to your Router logic
 } catch (Exception $e) {
     echo "Error: " . $e->getMessage();
     echo "Error: " . $e->getMessage();
