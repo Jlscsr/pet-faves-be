17595ea81726c0461e278fb1508eaebbd936c9fc
Daniel Garcia <danielg19990@gmail.com>
2024-12-11 12:05:39 +0800

    Asdasd

--- src/Configs/DatabaseConnection.php
+++ src/Configs/DatabaseConnection.php
@@ -11,7 +11,7 @@ class DatabaseConnection
     {
 
         try {
-            $environment = 'development';
+            $environment = 'production';
 
             if ($environment == "production") {
 
